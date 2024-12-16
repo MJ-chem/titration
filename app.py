13f1c0ffb64d286ce87eb6b7f7daeb5c51b8cae2
MJ-chem <mj200019@gmail.com>
2024-12-16 13:22:12 +0900

    app.py

--- app.py
+++ app.py
@@ -379,6 +379,8 @@ elif selected_page == "🌍 실생활 적용":
     with st.expander("", expanded=True):
         st.header("📈🔍 북태평양 하와이 마우나로아산과 하와이 근해 알로하 측정소에서 측정한 대기의")
         st.header("CO\u2082 농도와 해수의 pH")
+        st.markdown("<h1 style='text-indent: 30px;'>📈🔍 북태평양 하와이 마우나로아산과 하와이 근해 알로하 측정소에서 측정한 대기의 <br> CO₂ 농도와 해수의 pH </h1>", 
+            unsafe_allow_html=True)
         
         # 데이터 로드
         file_path = 'data.csv'
